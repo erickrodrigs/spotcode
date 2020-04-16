255af21d6f096dfc6c1a251c0b1da0a91dd02903
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-04-16 00:01:58 -0300

    Created first pages and discovery screen

---
+++ app/javascript/packs/spotcode.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../App'
+
+document.addEventListener('turbolinks:load', () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
